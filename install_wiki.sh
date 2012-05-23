99873fa4faea05a85da2f6d6f27226986c7940da
nfl <nfl@yelblu.(none)>
2012-05-23 17:30:26 +0200

    Comments were missing in the script install_wiki.sh ... Fix

--- install_wiki.sh
+++ install_wiki.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# This script installs or deletes a Mediawiki on your computer.
-# It requires a web server with PHP and a database running and mediawiki installed. As it changes some permissions file, it might need root privileges.
+# This script installs or deletes a MediaWiki on your computer.
+# It requires a web server with PHP and a database running and mediawiki installed.
+# As it changes some permissions file, it surely needs root privileges.
 # Please set the CONFIGURATION VARIABLES section below first.
 
 #
@@ -9,13 +10,20 @@
 #
 WIKI_DIR_NAME="wiki"                    # Name of the wiki's directory
 WIKI_DIR_INST="/var/www"                # Directory of the web server
-WIKI_INST_DIR="/var/lib/mediawiki/"     # Directory of mediawiki
+WIKI_INST_DIR="/var/lib/mediawiki/"     # Directory of mediawiki installation
 DB_NAME="wikidb"        # Name of the database
 DB_SU_LOGIN="root"      # Login of the superuser in the database
 DB_SU_PASSW="root"      # Password of the superuser in the database
 
+#
+# Function cmd_install()
+# Install a wiki in your web server directory.
+#
 cmd_install()
 {
+        # Copy the files of a wiki in the web server's directory.
+        # If LocalSettings.php exists, we should delete it so that the user
+        # we will asked to enter the configuration settings later.
         cp -R "$WIKI_INST_DIR" "$WIKI_DIR_INST/$WIKI_DIR_NAME"
         if [ -f "$WIKI_DIR_INST/$WIKI_DIR_NAME/LocalSettings.php" ]; then
                 rm "$WIKI_DIR_INST/$WIKI_DIR_NAME/LocalSettings.php"
@@ -23,15 +31,20 @@ cmd_install()
         chmod -R ugo+rw "$WIKI_DIR_INST/$WIKI_DIR_NAME"
         chmod +x "$WIKI_DIR_INST/$WIKI_DIR_NAME/config/"
 
+        # Ask the user to enter the configuration settings file until
+        # this one is in the wiki's directory
         is_not_set=1
         while [ "$is_not_set" -eq "1" ] ; do
                 read -p "Please, open http://localhost/$WIKI_DIR_NAME/config/index.php and set your configuration then send the form. Prompt any key when the form has been sent." yn
 
-                # The user might have already moved the WikiMedia's configuration
-                # file in the right folder. If not we must do it.
+                # The user might have already moved the wiki configuration settings
+                # file in the wiki's folder. If not we must do it.
                 if [ -f "$WIKI_DIR_INST/$WIKI_DIR_NAME/LocalSettings.php" ] ; then
                         is_not_set=0
                 else
+                        # The configuration settings file is not in the wiki folder. If the WikiMedia's form
+                        # has created it in config/ folder, we move it in wiki's directory. Otherwise, we
+                        # need to wait because the user prompt a key by mistake.
                         if [ -f "$WIKI_DIR_INST/$WIKI_DIR_NAME/config/LocalSettings.php" ] ; then
                                 cp "$WIKI_DIR_INST/$WIKI_DIR_NAME/config/LocalSettings.php" "$WIKI_DIR_INST/$WIKI_DIR_NAME"
                                 is_not_set=0
@@ -44,6 +57,11 @@ cmd_install()
         echo "Your wiki has been installed. You can check it at http://localhost/$WIKI_DIR_NAME"
 }
 
+#
+# Function cmd_clear()
+# Delete the wiki created in the web server's directory and all its content
+# saved in the database.
+#
 cmd_clear() {
         # Delete the wiki's directory
         rm -rf "$WIKI_DIR_INST/$WIKI_DIR_NAME"
@@ -58,28 +76,18 @@ cmd_clear() {
         rm .tmp
 }
 
-# Verify that services are running
-# TODO
-cmd_verify() {
-        # Apache with PHP
-        # MySQL
-        echo "Make sure your services (web server with PHP and database) are running"
-}
-
 cmd_help() {
         echo "Usage: "
-        echo "  ./install_wiki <install|clear|help>"
-        echo "          install: (TODO) Install a mediawiki on your computer."
-        echo "          clear: Delete your mediawiki installation."
+        echo "  ./install_wiki <install|delete|help>"
+        echo "          install: Install a wiki on your computer."
+        echo "          delete: Delete the wiki installation."
 }
 
-# Argument: install, clear, verify
+# Argument: install, delete
 if [ "$1" = "install" ] ; then
-        cmd_verify
         cmd_install
         exit 0
-elif [ "$1" = "clear" ] ; then
-        cmd_verify
+elif [ "$1" = "delete" ] ; then
         cmd_clear
         exit 0
 else
