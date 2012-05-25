68e0f4d151c29cdcfee59ff22bdefbbbc653697b
nfl <nfl@yelblu.(none)>
2012-05-25 22:18:24 +0200

    Script scripts_wiki/install_wiki.sh with SQLite3 done. Ready for feedbacks @git.

--- scripts_wiki/install_wiki.sh
+++ scripts_wiki/install_wiki.sh
@@ -8,67 +8,102 @@
 #
 # CONFIGURATION VARIABLES
 #
-WIKI_DIR_NAME="wiki"                    # Name of the wiki's directory
-WIKI_DIR_INST="/var/www"                # Directory of the web server
-WIKI_INST_DIR="/var/lib/mediawiki/"     # Directory of mediawiki installation
-DB_NAME="wikidb"        # Name of the database
-DB_SU_LOGIN="root"      # Login of the superuser in the database
-DB_SU_PASSW="password"      # Password of the superuser in the database
-DB_ADDRESS="localhost"
-DB_PORT="5432"
+WIKI_DIR_NAME="wiki"            # Name of the wiki's directory
+WIKI_DIR_INST="/var/www"        # Directory of the web server
+TMP="/tmp"                      # Temporary directory for downloads
+                                # Absolute address needed!
+SERVER_ADDR="localhost"         # Web server's address
+
+# Do not change the variables below
+MW_VERSION="mediawiki-1.19.0"
+DB_FILE="wikidb.sqlite"
+
+fail()
+{
+        echo $1
+        exit 1
+}
+
+#
+# Function grep_change_file()
+# Grep the first occurence of the parameter $1 value $2 and replace
+# that value by $3 in the MediaWiki's LocalSettings.php file named $4.
+#
+grep_change_file() {
+        param="$1"
+        value="$2"
+        replace="$3"
+        file="$4"
+        file_tmp="${file}.tmp"
+        
+        nline=`grep -n "^\$${param}" ${file} | cut -d ':' -f 1`
+        sed "${nline}s,${value},${replace}," "$file" > "$file_tmp"
+        cp "$file_tmp" "$file"
+        rm "$file_tmp"
+}
+
 #
 # Function cmd_install()
 # Install a wiki in your web server directory.
 #
-wiki_config()
-{
-wget -q --post-data "Sitename=TEST&EmergencyContact=webmaster%40localhost&LanguageCode=en&License=none&SysopName=user&SysopPass=password&SysopPass2=password&Shm=none&MCServers=&Email=email_enabled&Emailuser=emailuser_enabled&Enotif=enotif_allpages&Eauthent=eauthent_enabled&DBtype=mysql&DBserver=$DB_ADDRESS&DBname=$DB_NAME&DBuser=wikiuser&DBpassword=wikipass&DBpassword2=wikipass&useroot=on&RootUser=$DB_SU_LOGIN&RootPW=$DB_SU_PASSW&DBprefix=&DBengine=InnoDB&DBschema=mysql5-binary&DBport=$DB_PORT&DBmwschema=mediawiki&DBts2schema=public&SQLiteDataDir=&DBprefix2=&DBport_db2=50000&DBmwschema=mediawiki&DBcataloged=cataloged" http://localhost/wiki/config/index.php
-}
-
-
-
 cmd_install()
 {
-        # Copy the files of a wiki in the web server's directory.
-        # If LocalSettings.php exists, we should delete it so that the user
-        # we will asked to enter the configuration settings later.
-        cp -R "$WIKI_INST_DIR" "$WIKI_DIR_INST/$WIKI_DIR_NAME"
-        if [ "$?" -eq "1" ] ; then exit 1; fi
-        if [ -f "$WIKI_DIR_INST/$WIKI_DIR_NAME/LocalSettings.php" ]; then
-                rm "$WIKI_DIR_INST/$WIKI_DIR_NAME/LocalSettings.php"
-                if [ "$?" -eq "1" ] ; then exit 1; fi
+        # Copy the generic LocalSettings.php in the web server's directory
+        # And modify parameters according to the ones set at the top
+        # of this script.
+        # Note that LocalSettings.php is never modified.
+        if [ ! -f "LocalSettings.php" ] ; then
+                fail "Can't find LocalSettings.php in the current folder.
+                Please run the script inside its folder."
         fi
-        chmod -R ugo+rw "$WIKI_DIR_INST/$WIKI_DIR_NAME"
-        if [ "$?" -eq "1" ] ; then exit 1; fi
-        chmod +x "$WIKI_DIR_INST/$WIKI_DIR_NAME/config/"
-        if [ "$?" -eq "1" ] ; then exit 1; fi
+        cp "LocalSettings.php" "LocalSettings-tmp.php" ||
+                fail "Unable to copy LocalSettings.php to LocalSettings-tmp.php"
+        grep_change_file "wgScriptPath" "wiki" "$WIKI_DIR_NAME" "LocalSettings-tmp.php"
+        grep_change_file "wgServer" "localhost" "$SERVER_ADDR" "LocalSettings-tmp.php"
+        grep_change_file "wgSQLiteDataDir" '/tmp' "$TMP" "LocalSettings-tmp.php"
+        mkdir "$WIKI_DIR_INST/$WIKI_DIR_NAME"
+        if [ ! -d "$WIKI_DIR_INST/$WIKI_DIR_NAME" ] ; then
+                fail "Folder $WIKI_DIR_INST/$WIKI_DIR_NAME don't exist. Please create it
+                and launch the script again."
+        fi
+        mv "LocalSettings-tmp.php" "$WIKI_DIR_INST/$WIKI_DIR_NAME/LocalSettings.php" ||
+                fail "Unable to move LocalSettings-tmp.php in $WIKI_DIR_INST/$WIKI_DIR_NAME"
+        echo "File LocalSettings.php is set in $WIKI_DIR_INST/$WIKI_DIR_NAME"
 
-        # Ask the user to enter the configuration settings file until
-        # this one is in the wiki's directory
-        is_not_set=1
-        while [ "$is_not_set" -eq "1" ] ; do
-                
-		wiki_config
-		
-                # The user might have already moved the wiki configuration settings
-                # file in the wiki's folder. If not we must do it.
-                if [ -f "$WIKI_DIR_INST/$WIKI_DIR_NAME/LocalSettings.php" ] ; then
-                        is_not_set=0
-                else
-                        # The configuration settings file is not in the wiki folder. If the WikiMedia's form
-                        # has created it in config/ folder, we move it in wiki's directory. Otherwise, we
-                        # need to wait because the user prompt a key by mistake.
-                        if [ -f "$WIKI_DIR_INST/$WIKI_DIR_NAME/config/LocalSettings.php" ] ; then
-                                cp "$WIKI_DIR_INST/$WIKI_DIR_NAME/config/LocalSettings.php" "$WIKI_DIR_INST/$WIKI_DIR_NAME"
-                                if [ "$?" -eq "1" ] ; then exit 1; fi
-                                is_not_set=0
-                        else
-                                echo "$WIKI_DIR_INST/$WIKI_DIR_NAME/config/LocalSettings.php does not exist. You should wait that the form is sent."
-                        fi
-                fi
-        done
+        # Copy the database file in the TMP directory.
+        cmd_reset
+
+        # Fetch MediaWiki's archive if not already present in the TMP directory
+        cd $TMP
+        if [ ! -f "$MW_VERSION.tar.gz" ] ; then
+                echo "Downloading $MW_VERSION sources ..."
+                wget http://download.wikimedia.org/mediawiki/1.19/mediawiki-1.19.0.tar.gz ||
+                        fail "Unable to download http://download.wikimedia.org/mediawiki/1.19/mediawiki-1.19.0.tar.gz.
+                        Please fix your connection and launch the script again."
+        fi
+        tar xfz "$MW_VERSION.tar.gz"
+        echo "$MW_VERSION.tar.gz downloaded in `pwd`. You can delete it later if you want."
+
+        # Copy the files of MediaWiki wiki in the web server's directory.
+        cd "$MW_VERSION"
+        cp -Rf * "$WIKI_DIR_INST/$WIKI_DIR_NAME/" ||
+                fail "Unable to copy WikiMedia's files from `pwd` to $WIKI_DIR_INST/$WIKI_DIR_NAME"
 
         echo "Your wiki has been installed. You can check it at http://localhost/$WIKI_DIR_NAME"
+}
+
+#
+# Function cmd_reset()
+# Copy the initial database of the wiki over the actual one.
+#
+cmd_reset() {
+        if [ ! -f "$DB_FILE" ] ; then
+                fail "Can't find $DB_FILE in the current folder.
+                Please run the script inside its folder."
+        fi
+        cp "$DB_FILE" "$TMP" || fail "Can't copy wikidb.sqlite in $TMP"
+        chmod ugo+rw "$TMP/$DB_FILE" || fail "Can't add write perms on $TMP/$DB_FILE"
+        echo "File $DB_FILE is set in $TMP"
 }
 
 #
@@ -77,29 +112,28 @@ cmd_install()
 # saved in the database.
 #
 cmd_clear() {
-        # Delete the wiki's directory
-        rm -rf "$WIKI_DIR_INST/$WIKI_DIR_NAME"
+        # Delete the wiki's directory.
+        rm -rf "$WIKI_DIR_INST/$WIKI_DIR_NAME" ||
+                fail "Wiki's directory $WIKI_DIR_INST/$WIKI_DIR_NAME could not be deleted"
 
-        # Delete the database of the wiki
-        if [ -f ".tmp" ] ; then
-                echo "`pwd`/.tmp file exists, please delete it and launch the script again"
-                exit 1
-        fi
-        echo "DROP DATABASE $DB_NAME ;" > .tmp
-        mysql --user="$DB_SU_LOGIN" --password="$DB_SU_PASSW" "$DB_NAME" < .tmp
-        rm .tmp
+        # Delete the wiki's SQLite database
+        rm -f "$TMP/$DB_FILE" || fail "Database $TMP/$DB_FILE could not be deleted."
 }
 
 cmd_help() {
         echo "Usage: "
-        echo "  ./install_wiki <install|delete|help>"
+        echo "  ./install_wiki <install|reset|delete|help>"
         echo "          install: Install a wiki on your computer."
-        echo "          delete: Delete the wiki installation."
+        echo "          reset: Clear all pages and content of the wiki"
+        echo "          delete: Delete the wiki and all its pages and content"
 }
 
-# Argument: install, delete
+# Argument: install, reset, delete
 if [ "$1" = "install" ] ; then
         cmd_install
+        exit 0
+elif [ "$1" = "reset" ] ; then
+        cmd_reset
         exit 0
 elif [ "$1" = "delete" ] ; then
         cmd_clear
